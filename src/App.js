402336dfc6374f04870cf451aaed18670ad6fb3b
Nontagorn Chantarasena <nontagorn.c@gmail.com>
2023-02-17 23:24:01 +0700

    styling css and set up background project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import { Button } from "antd";
+import { Button, Image } from "antd";
+import pokemonLogoS from "./assets/images/pokedex.png";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Image width={200} src={pokemonLogoS} />
       <Button type="primary">Click me</Button>
     </div>
   );
