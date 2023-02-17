64803d4f571234dddfd89ac4a097f812fd5d6a69
Nontagorn Chantarasena <nontagorn.c@gmail.com>
2023-02-17 22:50:30 +0700

    Install ui dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "antd";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Button type="primary">Click me</Button>
     </div>
   );
 }
